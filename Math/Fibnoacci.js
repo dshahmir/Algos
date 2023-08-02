c90be53186b9441a4a7fc6e26abef2e8c7448237
shamir <shahmirx5@gmail.com>
2023-08-02 15:00:57 +0500

    fibonaccci series algo

--- Math/Fibnoacci.js
+++ Math/Fibnoacci.js
@@ -1,1 +1,10 @@
-console.log("Hello")
+const fibnacci = (n) => {
+  const res = [0, 1];
+
+  for (let i = 2; i < n; i++) {
+    res[i] = res[i - 1] + res[i - 2];
+  }
+  return res;
+};
+
+console.log(fibnacci(4)); // time complexity O(n)
